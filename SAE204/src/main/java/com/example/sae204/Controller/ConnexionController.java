004de5c8c0fda9676d506b86f088d78c54475258
Samuel112003 <107188175+Samuel112003@users.noreply.github.com>
2022-06-12 01:30:25 +0200

    petite correction pour ne pas se connecter sans mot de passe

--- SAE204/src/main/java/com/example/sae204/Controller/ConnexionController.java
+++ SAE204/src/main/java/com/example/sae204/Controller/ConnexionController.java
@@ -36,56 +36,53 @@ public class ConnexionController extends Controller{
 
     public void onConnexionButtonClick() throws SQLException, IOException, ClassNotFoundException {
 
-        String EnterId=IDTextField.getText();// stockage de l'id entré
-        String EnterPwd=PasswordTextField.getText(); // stockage du password entré
+        String EnterId = IDTextField.getText();// stockage de l'id entré
+        String EnterPwd = PasswordTextField.getText(); // stockage du password entré
 
-       if (EnterId.equals("")) {
-           loginMessageLabel.setText("Veuillez entrez un Identifiant"); // si l' ID est vide alors msg erreur
-       }
-       if (EnterPwd.equals("")){
-           loginMessageLabel.setText("Veuillez entez un mot de passe "); // si le mdp est vide alors msg erreur
-       }
-        if(EnterId.equals("") && EnterPwd.equals("")) {
-           loginMessageLabel.setText("Veuillez entrez un identifiant et mot de passe "); // si les 2 sont vides alors msg erreur
+        if (EnterId.equals("")) {
+            loginMessageLabel.setText("Veuillez entrez un Identifiant"); // si l' ID est vide alors msg erreur
         }
+        if (EnterPwd.equals("")) {
+            loginMessageLabel.setText("Veuillez entez un mot de passe "); // si le mdp est vide alors msg erreur
+        }
+        if (EnterId.equals("") && EnterPwd.equals("")) {
+            loginMessageLabel.setText("Veuillez entrez un identifiant et mot de passe "); // si les 2 sont vides alors msg erreur
+        } else {
+            //EtudiantAPK.myjdbc.connect("root",""); // connection a la base
+            String result1 = "", result2 = "";
+            LinkedList<Etudiant> listEtu = DAO.listerEtu();
 
-
-        //EtudiantAPK.myjdbc.connect("root",""); // connection a la base
-        String result1="", result2="";
-        LinkedList<Etudiant> listEtu=DAO.listerEtu();
-
-        if(AccueilController.etatButton=="E"){
-            for (Etudiant etu : listEtu) {
-                if(etu.getNum_etu().equals(EnterId)) {
-                    result1 = etu.getMdp_etu();
+            if (AccueilController.etatButton == "E") {
+                for (Etudiant etu : listEtu) {
+                    if (etu.getNum_etu().equals(EnterId)) {
+                        result1 = etu.getMdp_etu();
+                    }
                 }
+                System.out.println("result1 :" + result1);
+            } else if (AccueilController.etatButton == "P") {
+                String query2 = "SELECT MDP_per FROM PERSONNEL WHERE Harpege='" + EnterId + "';";
+                result2 = EtudiantAPK.myjdbc.executeReadQuery(query2);// stockage de la requête
             }
-            System.out.println("result1 :"+result1);
-        }
-        else if(AccueilController.etatButton=="P"){
-           String  query2="SELECT MDP_per FROM PERSONNEL WHERE Harpege='" + EnterId + "';" ;
-             result2= EtudiantAPK.myjdbc.executeReadQuery(query2);// stockage de la requête
-        }
 
-              // execute la requete et stocke le resultat
+            // execute la requete et stocke le resultat
 
-        if(EnterPwd.equals(result1)){ // verife si le mdp dans la base correspond au mdp entré
-            GoToPage("EtudiantView.fxml", "Etudiant_Connexion");
-        }
-        else if(EnterPwd.equals(result2)){
-            GoToPage("","");// page pour le personnel
-        }
-        else {
-            loginMessageLabel.setText("Identifiant ou mot de passe invalide");
+            if (EnterPwd.equals(result1)) { // verife si le mdp dans la base correspond au mdp entré
+                GoToPage("EtudiantView.fxml", "Etudiant_Connexion");
+            } else if (EnterPwd.equals(result2)) {
+                GoToPage("", "");// page pour le personnel
+            } else {
+                loginMessageLabel.setText("Identifiant ou mot de passe invalide");
+            }
         }
     }
-    public void retour(ActionEvent event){
-        if(ChoixRoleController.choixRole.equals("E")){
-            GoToPage("Accueil.fxml", "Accueil");
-        }
-        else{
-            GoToPage("ChoixRole.fxml", "Choix du rôle");
+        public void retour (ActionEvent event){
+            if (ChoixRoleController.choixRole.equals("E")) {
+                GoToPage("Accueil.fxml", "Accueil");
+            }
+            else {
+                GoToPage("ChoixRole.fxml", "Choix du rôle");
+            }
         }
     }
 
-}
+
