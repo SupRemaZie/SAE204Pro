546413501e86f9f3cbbeb0e27fc04218d12e7ce3
Samuel112003 <107188175+Samuel112003@users.noreply.github.com>
2022-06-11 11:28:20 +0200

    Modification de l'APK + création d'une nouvelle page d'accueil avec un bouton dans la première page de connection pour faire un racourci vers cette nouvelle page

---
+++ SAE204/src/main/java/com/example/sae204/Controller/AccueilController.java
@@ -0,0 +1,26 @@
+package com.example.sae204.Controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AccueilController {
+
+    @FXML
+    private Button bouton_etu;
+
+    @FXML
+    private Button bouton_per;
+
+    @FXML
+    void bouton_etu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bouton_per(ActionEvent event) {
+
+    }
+}
+
