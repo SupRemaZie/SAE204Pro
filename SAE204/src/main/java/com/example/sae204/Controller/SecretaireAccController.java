797af15f54e1245df01ec53ead99978351969fa7
Damien PORCHET <104629525+SupRemaZie@users.noreply.github.com>
2022-06-12 18:28:05 +0200

    Choix du groupe parent

--- SAE204/src/main/java/com/example/sae204/Controller/SecretaireAccController.java
+++ SAE204/src/main/java/com/example/sae204/Controller/SecretaireAccController.java
@@ -1,6 +1,8 @@
 package com.example.sae204.Controller;
 
+import com.example.sae204.EtudiantAPK;
 import com.example.sae204.Modele.DAO;
+import com.example.sae204.Modele.Etudiant;
 import com.example.sae204.Modele.Groupe;
 import com.example.sae204.Modele.Promotion;
 import javafx.beans.value.ChangeListener;
@@ -13,12 +15,16 @@ import javafx.scene.control.*;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class SecretaireAccController extends Controller implements Initializable {
 
     @FXML
     private Label GroupeSelected;
+    @FXML
+    private ListView<String> GrouParentList;
 
     private String CurrentGroupe;
 
@@ -27,8 +33,7 @@ public class SecretaireAccController extends Controller implements Initializable
     @FXML
     private ListView<String> GroupeList;
 
-    @FXML
-    private ListView<String> GrpParentList;
+
 
     @FXML
     void onDisconnectButtonClick(ActionEvent event) {
@@ -38,20 +43,27 @@ public class SecretaireAccController extends Controller implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            for(Groupe grp : DAO.listerGrp()){
-                GroupeList.getItems().add(grp.toString());
-            }
+            ChampGroupe();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        GroupeList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+    }
+
+    void ChampGroupe () throws SQLException, ClassNotFoundException {
+        GrouParentList.getItems().addAll(DAO.ListGrpParent());
+
+        GrouParentList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                CurrentGroupe=GroupeList.getSelectionModel().getSelectedItem();
-                GroupeSelected.setText(CurrentGroupe);
+                CurrentGroupe = GrouParentList.getSelectionModel().getSelectedItem();
+
+                ParentSelected.setText(CurrentGroupe);
+
+
             }
         });
     }
+
 }
