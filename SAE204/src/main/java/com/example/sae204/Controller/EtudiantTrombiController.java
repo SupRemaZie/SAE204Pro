3826ca61905d9a6f54876dd01e79198930e07181
Samuel112003 <107188175+Samuel112003@users.noreply.github.com>
2022-06-14 21:31:29 +0200

    Le TROMBI !

--- SAE204/src/main/java/com/example/sae204/Controller/EtudiantTrombiController.java
+++ SAE204/src/main/java/com/example/sae204/Controller/EtudiantTrombiController.java
@@ -26,6 +26,7 @@ public class EtudiantTrombiController extends Controller implements Initializabl
 
     public static String num_etu;
 
+    public String groupeParent;
     //Déclaration des images du trombinoscope
 
     private static String CurrentGroupeParent;
@@ -186,6 +187,7 @@ public class EtudiantTrombiController extends Controller implements Initializabl
     private Label adressemaillabel;
 
     private LinkedList<Groupe> listGroup= DAOGroupe.listerGrp();
+    LinkedList<Etudiant> listEtu=new LinkedList<>();
 
     public EtudiantTrombiController() throws MalformedURLException, SQLException, ClassNotFoundException {
     }
@@ -198,16 +200,6 @@ public class EtudiantTrombiController extends Controller implements Initializabl
     public void rechercherEtu(ActionEvent event){
 
     }
-    @FXML
-    public void afficherImage(ActionEvent event){
-        try {
-            image1.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/ElisabethBlanchard.png")));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        cacherPhoto(4);
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String mail ="";
@@ -225,6 +217,7 @@ public class EtudiantTrombiController extends Controller implements Initializabl
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        cacherPhoto(24);
 
 
     }
@@ -352,6 +345,363 @@ public class EtudiantTrombiController extends Controller implements Initializabl
 
 
     }
+    public void montrerPhoto(int n){
+        Etudiant etu;
+        if(n>0) {
+            image1.setOpacity(100);
+            prenom1.setOpacity(100);
+            nom1.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image1.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom1.setText(etu.getPrenom_etu());
+            nom1.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>1) {
+            image2.setOpacity(100);
+            prenom2.setOpacity(100);
+            nom2.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image2.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom2.setText(etu.getPrenom_etu());
+            nom2.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>2) {
+            image3.setOpacity(100);
+            prenom3.setOpacity(100);
+            nom3.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image3.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom3.setText(etu.getPrenom_etu());
+            nom3.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>3) {
+            image4.setOpacity(100);
+            prenom4.setOpacity(100);
+            nom4.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image4.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom4.setText(etu.getPrenom_etu());
+            nom4.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>4) {
+            image5.setOpacity(100);
+            prenom5.setOpacity(100);
+            nom5.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image5.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom5.setText(etu.getPrenom_etu());
+            nom5.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>5) {
+            image6.setOpacity(100);
+            prenom6.setOpacity(100);
+            nom6.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image6.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom6.setText(etu.getPrenom_etu());
+            nom6.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>6) {
+            image7.setOpacity(100);
+            prenom7.setOpacity(100);
+            nom7.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image7.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom7.setText(etu.getPrenom_etu());
+            nom7.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>7) {
+            image8.setOpacity(100);
+            prenom8.setOpacity(100);
+            nom8.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image8.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom8.setText(etu.getPrenom_etu());
+            nom8.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>8) {
+            image9.setOpacity(100);
+            prenom9.setOpacity(100);
+            nom9.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image9.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom9.setText(etu.getPrenom_etu());
+            nom9.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>9) {
+            image10.setOpacity(100);
+            prenom10.setOpacity(100);
+            nom10.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image10.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom10.setText(etu.getPrenom_etu());
+            nom10.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>10) {
+            image11.setOpacity(100);
+            prenom11.setOpacity(100);
+            nom11.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image11.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom11.setText(etu.getPrenom_etu());
+            nom11.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>11) {
+            image12.setOpacity(100);
+            prenom12.setOpacity(100);
+            nom12.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image12.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom12.setText(etu.getPrenom_etu());
+            nom12.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>12) {
+            image13.setOpacity(100);
+            prenom13.setOpacity(100);
+            nom13.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image13.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom13.setText(etu.getPrenom_etu());
+            nom13.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>13) {
+            image14.setOpacity(100);
+            prenom14.setOpacity(100);
+            nom14.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image14.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom14.setText(etu.getPrenom_etu());
+            nom14.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>14) {
+            image15.setOpacity(100);
+            prenom15.setOpacity(100);
+            nom15.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image15.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom15.setText(etu.getPrenom_etu());
+            nom15.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>15) {
+            image16.setOpacity(100);
+            prenom16.setOpacity(100);
+            nom16.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image16.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom16.setText(etu.getPrenom_etu());
+            nom16.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>16) {
+            image17.setOpacity(100);
+            prenom17.setOpacity(100);
+            nom17.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image17.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom17.setText(etu.getPrenom_etu());
+            nom17.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>17) {
+            image18.setOpacity(100);
+            prenom18.setOpacity(100);
+            nom18.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image18.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom18.setText(etu.getPrenom_etu());
+            nom18.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>18) {
+            image19.setOpacity(100);
+            prenom19.setOpacity(100);
+            nom19.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image19.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom19.setText(etu.getPrenom_etu());
+            nom19.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>19) {
+            image20.setOpacity(100);
+            prenom20.setOpacity(100);
+            nom20.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image20.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom20.setText(etu.getPrenom_etu());
+            nom20.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>20) {
+            image21.setOpacity(100);
+            prenom21.setOpacity(100);
+            nom21.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image21.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom21.setText(etu.getPrenom_etu());
+            nom21.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>21) {
+            image22.setOpacity(100);
+            prenom22.setOpacity(100);
+            nom22.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image22.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom22.setText(etu.getPrenom_etu());
+            nom22.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>22) {
+            image23.setOpacity(100);
+            prenom23.setOpacity(100);
+            nom23.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image23.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom23.setText(etu.getPrenom_etu());
+            nom23.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+        if(n>23) {
+            image24.setOpacity(100);
+            prenom24.setOpacity(100);
+            nom24.setOpacity(100);
+            etu=listEtu.poll();
+            try {
+                image24.setImage(new Image(new FileInputStream("SAE204/src/main/resources/com/example/sae204/image/PhotoEtudiant/"+etu.getPhoto_etu())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            prenom24.setText(etu.getPrenom_etu());
+            nom24.setText(etu.getNom_etu());
+            listEtu.add(etu);
+        }
+
+
+    }
+
+    private  int verif(){
+
+        if(!(listGroupe.getValue()==null|| listGroupe.getValue().equals("AUCUN"))&&(listGroupeEnfant.getValue()==null|| listGroupeEnfant.getValue().equals("AUCUN"))){
+
+            return 0;
+
+        }
+        else if(!(listGroupe.getValue()==null|| listGroupe.getValue().equals("AUCUN"))&&!(listGroupeEnfant.getValue()==null|| listGroupeEnfant.getValue().equals("AUCUN"))){
+
+            return 2;
+        }
+
+        return 1;
+
+    }
 
     void ChampGroupeParent () throws SQLException, ClassNotFoundException {
         listGroupe.getItems().addAll(DAOGroupe.ListGrpParent());
@@ -359,11 +709,20 @@ public class EtudiantTrombiController extends Controller implements Initializabl
         listGroupe.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                    CurrentGroupeParent = listGroupe.getSelectionModel().getSelectedItem();
+                if(verif()==0){
+                    CurrentGroupeParent = String.valueOf(listGroupe.getValue());
+                }
+                else if (verif()==2){
+                    CurrentGroupeParent=String.valueOf(listGroupeEnfant.getValue());
+                }
+                else {
+                    System.out.println("err ");
+                }
 
                 System.out.println();
                 try {
-                    LinkedList<Etudiant> listEtu = chercherEtuGroupePromo(CurrentGroupeParent);
+                    listEtu = chercherEtuGroupePromo(CurrentGroupeParent);
+                    afficherPhoto(listEtu);
                     for (Etudiant etu : listEtu) {
                         System.out.println(etu.getNom_etu());
                     }
@@ -386,11 +745,21 @@ public class EtudiantTrombiController extends Controller implements Initializabl
                     e.printStackTrace();
                 }
                 listGroupeEnfant.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
                     @Override
                     public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                        CurrentGroupe = listGroupeEnfant.getSelectionModel().getSelectedItem();
+                        if(verif()==0){
+                            CurrentGroupe = String.valueOf(listGroupe.getValue());
+                        }
+                        else if (verif()==2){
+                            CurrentGroupe=String.valueOf(listGroupeEnfant.getValue());
+                        }
+                        else {
+                            System.out.println("err ");
+                        }
                         try {
-                            LinkedList<Etudiant> listEtu = chercherEtuGroupePromo(CurrentGroupe);
+                            listEtu = chercherEtuGroupePromo(CurrentGroupe);
+                            afficherPhoto(listEtu);
 
                             System.out.println();
                             for (Etudiant etu : listEtu) {
@@ -406,13 +775,8 @@ public class EtudiantTrombiController extends Controller implements Initializabl
             }
         });
     }
-    public void afficherPhoto(){
-        String leGroupe;
-        if(CurrentGroupe.equals(null)){
-            leGroupe=CurrentGroupeParent;
-        }
-        else{
-            leGroupe=CurrentGroupe;
-        }
+    public void afficherPhoto(LinkedList<Etudiant> listEtu){
+        cacherPhoto(24-listEtu.size());
+        montrerPhoto(listEtu.size());
     }
 }
